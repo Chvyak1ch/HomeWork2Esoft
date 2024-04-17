5e026127c376936d02f64643789d9c06d45d721d
Kostya chernyy <Kostia_2001@mail.ru>
2024-04-18 00:24:45 +0500

    отправка первых шаблонов

---
+++ indexJS.js
@@ -0,0 +1,11 @@
+ReactDOM.render(<div>
+    <h1>Привет!</h1>
+    <h2>hello</h2>
+    </div>, 
+document.getElementById("app"))
+
+ReactDOM.render(<div>
+    <img src = "Foto2.jpg"></img>
+    <h2>Черный Константин Викторович</h2>
+    </div>, 
+document.getElementById("bigCard"))
